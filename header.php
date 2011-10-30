34da3c63afdbeed14d84b60012880619ef12512c
John Clarke Mills <johnclarkemills@mac.com>
2011-10-30 15:06:18 -0700

    adding twitter link and some more cleanup

--- header.php
+++ header.php
@@ -23,20 +23,18 @@
 					<li><a href="http://feeds2.feedburner.com/SanFranVic">Subscribe via RSS</a></li>
 					<li class="last"><a href="http://feedburner.google.com/fb/a/mailverify?uri=SanFranVic&amp;loc=en_US">Subscribe via Email</a></li>
 				</ul>
-				<iframe id="fb" src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FSan-Fran-Vic%2F151489671584023&amp;layout=standard&amp;show_faces=true&amp;width=450&amp;action=like&amp;colorscheme=light" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+				<iframe id="fb" src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FSan-Fran-Vic%2F151489671584023&amp;layout=standard&amp;show_faces=true&amp;width=450&amp;action=like&amp;colorscheme=light" scrolling="no" frameborder="0" allowTransparency="true" height="70"></iframe>
 				</span>
 			</div>
 			<div id="mainNav">
 				<ul>
 					<li<?php echo ' class="first"' ?>><a href="<?php bloginfo('url'); ?>/">Home</a></li>
 					<?php wp_list_pages('title_li='); ?>
-				</ul>								
-				<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/" class="headSearch">
-					<div>
-						<input type="hidden" name="IncludeBlogs" id="IncludeBlogs" value="1"/>
-						<input type="text" class="keyword" onblur="if (document.forms['searchform'].s.value == '') document.forms['searchform'].s.value='Search Keywords';" onfocus="document.forms['searchform'].s.value='';" value="Search Keywords" name="s"/>
-						<input type="submit" value="" id="searchsubmit" class="searchButton" />
-					</div>
-				</form>
+				</ul>
+				
+				<div style="float: right; width: 250px;">
+					<a href="https://twitter.com/johnclarkemills" data-link-color="#FFFFFF" class="twitter-follow-button">Follow @johnclarkemills</a>
+					<script src="//platform.twitter.com/widgets.js" type="text/javascript"></script
+				</div>
 			</div>
 			<!-- Header End-->
